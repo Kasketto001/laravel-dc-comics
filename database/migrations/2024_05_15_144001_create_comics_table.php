a546489c54425ad626c7a684c0de3d0b0daf77f6
Kasketto001 <kaskettomaledetto@gmail.com>
2024-05-15 16:53:17 +0200

    Add Comic Seeder

--- database/migrations/2024_05_15_144001_create_comics_table.php
+++ database/migrations/2024_05_15_144001_create_comics_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->decimal('price', 8, 2);
+            $table->string('series')->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type');
             $table->timestamps();
         });
     }
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $comics = config('db.comics');
+
+        foreach ($comics as $comic) {
+
+            $item = new Comic();
+            $item->title = $comic['title'];
+            $item->description = $comic['description'];
+            $item->thumb = $comic['thumb'];
+            $item->price = $comic['price'];
+            $item->sale_date = $comic['sale_date'];
+            $item->type = $comic['type'];
+            $item->save();
+
+        }
     }
 }
